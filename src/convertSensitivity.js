c24be3c050785ed0868f5667a8986117f7ddabb7
kotay <2117654+kotay@users.noreply.github.com>
2017-11-21 08:55:52 +0000

    Update convertSensitivity.js

--- src/convertSensitivity.js
+++ src/convertSensitivity.js
@@ -1,7 +1,9 @@
-import { SENSITIVITY_LOW, SENSITIVITY_NORMAL, SENSITIVITY_HIGH } from './constants';
+import { SENSITIVITY_VERY_LOW, SENSITIVITY_LOW, SENSITIVITY_NORMAL, SENSITIVITY_HIGH } from './constants';
 
 export default function convertSensitivity(sensitivity) {
   switch (sensitivity) {
+    case SENSITIVITY_VERY_LOW:
+      return 180;
     case SENSITIVITY_LOW:
       return 120;
     case SENSITIVITY_HIGH:
